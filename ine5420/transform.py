14bd7a4132f22614f8298f067555b3812c765f23
Gabriel Simonetto <GabrielSimonetto@users.noreply.github.com>
2022-04-28 12:04:52 -0300

    Add x and y vieport transformations

---
+++ ine5420/transform.py
@@ -0,0 +1,24 @@
+def x_viewport(
+    x_window,
+    x_window_min,
+    x_window_max,
+    x_viewport_min,
+    x_viewport_max
+):
+    return (
+        (x_window - x_window_min) / (x_window_max - x_window_min)
+        * (x_viewport_max - x_viewport_min)
+    )
+
+
+def y_viewport(
+    y_window,
+    y_window_min,
+    y_window_max,
+    y_viewport_min,
+    y_viewport_max
+):
+    return (
+        (1 - ((y_window - y_window_min) / y_window_max - y_window_min))
+        * (y_viewport_max - y_viewport_min)
+    )
